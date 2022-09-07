26b8782a56f6f76f8be1e9ece5d89b3ad3ec22d5
Ultimatehero <fbthari@gmail.com>
2022-09-07 12:33:03 +0530

    Sorting techniques
    Intially selection sort

---
+++ Sort_techs/selectionsort.py
@@ -0,0 +1,20 @@
+#selection sort
+'''
+Find the minimum element in the array and swap it with the element in the beginning of array
+Later from there again find minimum element of the array and swap it and add the element to the 2nd position in the array
+and so on upto n elements
+'''
+
+array = [13,46,24,52,20,9]
+
+for index,ele in enumerate(array):
+    min_index = index
+    #Intially assume the minimum_element is in the current index
+    for j in range(index+1,len(array)):
+        #for i + 1 checking if the element is less than element in min_index
+        if(array[j] < array[min_index]):
+            min_index = j
+    #swap the elements
+    array[index],array[min_index] = array[min_index],array[index]
+
+print(array) 
